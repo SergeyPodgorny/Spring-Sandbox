dd7d3e0547828990f980516f9948783513bbe0e6
SergeyPodgorny <serkonpod@gmail.com>
2022-05-02 10:36:41 +0300

    refactoring

--- beanconfig/src/main/java/com/bean/config/beanconfig/controllers/JavaConfigController.java
+++ beanconfig/src/main/java/com/bean/config/beanconfig/controllers/JavaConfigController.java
@@ -1,11 +1,26 @@
 package com.bean.config.beanconfig.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bean.config.beanconfig.javaconfig.services.JavaConfigService;
 
 @RestController
 public class JavaConfigController {
 	
 	
+	JavaConfigService javaConfigService;
+	
+	@Autowired
+	public JavaConfigController(JavaConfigService javaConfigService) {
+		this.javaConfigService = javaConfigService;
+	}
+	
+	@GetMapping("helloJava")
+	public String messagePrinter() {
+		return javaConfigService.helloMessage();
+	}
 	
 	
 
---
+++ beanconfig/src/main/java/com/bean/config/beanconfig/javaconfig/services/JavaConfigService.java
@@ -0,0 +1,11 @@
+package com.bean.config.beanconfig.javaconfig.services;
+
+public class JavaConfigService {
+	
+	
+	public String helloMessage() {
+		return "hello from java configuration!";
+	}
+	
+
+}
