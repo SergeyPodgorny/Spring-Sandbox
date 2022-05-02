6c1ecc76f60b1c43ec8012d679d5d74b52b5be5c
SergeyPodgorny <serkonpod@gmail.com>
2022-05-02 08:20:25 +0300

    added dependencies and H2 DB connection

--- beanconfig/src/main/java/com/bean/config/beanconfig/BeanconfigApplication.java
+++ beanconfig/src/main/java/com/bean/config/beanconfig/BeanconfigApplication.java
@@ -2,8 +2,10 @@ package com.bean.config.beanconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:application-context.xml")
 public class BeanconfigApplication {
 
 	public static void main(String[] args) {
