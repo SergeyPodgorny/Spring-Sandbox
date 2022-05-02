c967871de4fa61216999430b49f9983826ff4691
SergeyPodgorny <serkonpod@gmail.com>
2022-05-02 08:09:04 +0300

    working on bean defenition approaches

---
+++ beanconfig/src/main/java/com/bean/config/beanconfig/controllers/XMLController.java
@@ -0,0 +1,31 @@
+package com.bean.config.beanconfig.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bean.config.beanconfig.services.XMLService;
+
+
+
+
+@RestController
+public class XMLController {
+
+	
+	
+	private final XMLService xmlService;
+	
+	@Autowired
+	public XMLController(XMLService xmlService) {
+		this.xmlService = xmlService;
+	}
+
+
+	@GetMapping("/helloXML")
+	public String helloXML() {
+		return xmlService.printMessage();
+	}
+	
+	
+}
---
+++ beanconfig/src/main/java/com/bean/config/beanconfig/services/XMLService.java
@@ -0,0 +1,11 @@
+package com.bean.config.beanconfig.services;
+
+public class XMLService {
+	
+	public String printMessage() {
+		return "Hello XML!";
+	}
+	
+	
+
+}
