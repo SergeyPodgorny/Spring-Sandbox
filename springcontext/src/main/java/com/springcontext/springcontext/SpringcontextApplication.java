2e63b577d2fbc2e2a3a48997954f9b95ff4bf72c
SergeyPodgorny <serkonpod@gmail.com>
2022-05-09 05:47:17 +0300

    initial commit for springcontext demo

---
+++ springcontext/src/main/java/com/springcontext/springcontext/SpringcontextApplication.java
@@ -0,0 +1,13 @@
+package com.springcontext.springcontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcontextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcontextApplication.class, args);
+	}
+
+}
---
+++ springcontext/src/test/java/com/springcontext/springcontext/SpringcontextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcontext.springcontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcontextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
