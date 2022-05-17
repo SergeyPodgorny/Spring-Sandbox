03b7b7b996d921ecd3f277b71971cdd9cbc893fe
SergeyPodgorny <serkonpod@gmail.com>
2022-05-17 18:48:15 +0300

    refactoring

--- hibernate/src/main/java/com/hibernate/configuration/entity/AccountDAO.java
+++ hibernate/src/main/java/com/hibernate/configuration/entity/AccountDAO.java
@@ -5,10 +5,12 @@ package com.hibernate.configuration.entity;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class AccountDAO {
 
 	
@@ -35,6 +37,11 @@ public class AccountDAO {
 	public Account findById(Long id) {
 		Session session = sessionFactory.openSession();
 		Account account = session.find(Account.class,id);
+		System.out.println(account);
+		System.out.println("\n\n");
+		System.out.println(account.getBill());
+		
+		
 		session.close();
 		return account;	
 	}
