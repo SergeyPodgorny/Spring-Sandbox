43d361f28f1a30d2d2b6546c5c1d3e5ed8b3276f
SergeyPodgorny <serkonpod@gmail.com>
2022-05-22 11:08:10 +0300

    trying to make controller work

---
+++ SpringMVC/src/com/springmvc/controller/BasicController.java
@@ -0,0 +1,32 @@
+package com.springmvc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+	
+	
+	
+	
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
+		return "hello";
+	}
+	
+	
+	@RequestMapping(value = "/hello2", method = RequestMethod.POST)
+	public String hello2() {
+		return "hello";
+	}
+	
+	
+	
+	
+	
+	
+	
+}
