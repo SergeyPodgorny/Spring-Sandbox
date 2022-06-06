3b262192d4bcf5653eff5d16e23b2d7b8a8cd7e8
SergeyPodgorny <serkonpod@gmail.com>
2022-06-06 17:05:08 +0300

    finallizing spring sandbox

---
+++ SpringCloudDemo/registry/src/main/java/com/springcloud/demo/registry/RegistryApplication.java
@@ -0,0 +1,14 @@
+package com.springcloud.demo.registry;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
