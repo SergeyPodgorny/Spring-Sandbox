54b36d229c1dec326620d4fa166e68f4a5b9d671
SergeyPodgorny <serkonpod@gmail.com>
2022-07-27 17:16:42 +0300

    logging deployed

--- caching/src/main/java/com/caching/service/GameService.java
+++ caching/src/main/java/com/caching/service/GameService.java
@@ -1,15 +1,24 @@
 package com.caching.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
+
+import com.caching.CachingApplication;
 import com.caching.entity.Game;
+import com.caching.repository.GameRepository;
 import com.caching.repository.GameRepositoryImpl;
+
 @Component
 @CacheConfig(cacheNames = "games")
 public class GameService {
 
+	
+	
+	private static final Logger logger = LoggerFactory.getLogger(CachingApplication.class);
 	
 	private GameRepositoryImpl gameRepositoryImpl;
 	
@@ -20,7 +29,13 @@ public class GameService {
 
 	@Cacheable
 	public Game findGameById(String id) {
-		return gameRepositoryImpl.findGameById(id);
+		
+		logger.info("service started");
+		Game foundEntry = gameRepositoryImpl.findGameById(id); 
+		logger.info("service terminated");
+		return foundEntry;
+		
+		
 	}
 	
 	
