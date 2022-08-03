e2a8c18594d26f8fa0592a513d83f258347ce864
SergeyPodgorny <serkonpod@gmail.com>
2022-08-03 20:29:08 +0300

    added new migration

--- security/src/main/java/com/security/jwt_utils/TokenManager.java
+++ security/src/main/java/com/security/jwt_utils/TokenManager.java
@@ -1,18 +1,43 @@
 package com.security.jwt_utils;
 
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys; 
 @Component
-public class TokenManager implements Serializable{
+public class TokenManager {
 
-	
-	
 	
 	@Value("${secret}")
 	private String jwtSecret;
-	private static final long serialVersionUID = 1L;
+	@Value("${expiration_time}")
+	private long token_exp_time;
+	
+	
+	
+	
+	public String generateToken(UserDetails userService) {
+		Map<String, Object> claims = new HashMap<>();
+		return Jwts.builder().setClaims(claims)
+				.setSubject(userService.getUsername())
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis()+token_exp_time)).signWith(convertStringToKey(jwtSecret)).compact();
+	}
+	
+	
+	private Key convertStringToKey(String secret) {
+		byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+	
 	
 	
 	
