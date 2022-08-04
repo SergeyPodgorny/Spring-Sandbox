b618ba247d0156fc56e5cea6bf40b86149d05563
SergeyPodgorny <serkonpod@gmail.com>
2022-08-04 18:48:31 +0300

    working on loading user from db

---
+++ security/src/main/java/com/security/service/JwtUserDetailsService.java
@@ -0,0 +1,47 @@
+package com.security.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService{
+
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	@Service
+//	public class JwtUserDetailsService implements UserDetailsService { 
+//	   @Override 
+//	   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//	      if ("randomuser123".equals(username)) { 
+//	         return new User("randomuser123", 
+//	            "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", 
+//	            new ArrayList<>()); 
+//	      } else { 
+//	         throw new UsernameNotFoundException("User not found with username: " + username); 
+//	      } 
+//	   } 
+//	}
+	
+	
+	
+	
+	
+	
+	
+}
