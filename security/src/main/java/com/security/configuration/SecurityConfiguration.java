06e714887ae639a209670e98f4724d69f0d8e190
SergeyPodgorny <serkonpod@gmail.com>
2022-08-06 11:42:37 +0300

    removing devtools from classpath

--- security/src/main/java/com/security/configuration/SecurityConfiguration.java
+++ security/src/main/java/com/security/configuration/SecurityConfiguration.java
@@ -1,24 +1,54 @@
 package com.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.security.jwt_utils.JwtAuthenticationEntryPoint;
+import com.security.jwt_utils.JwtFilter;
+
+@Configuration
 public class SecurityConfiguration {
+
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	@Autowired
+	private JwtFilter filter;
 	
+	  	
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{		
-		return null;
-	}
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		
+		http
+		.authorizeRequests()
+			.antMatchers("/start_page", "login").permitAll();
 	
+		return http.build();
+		}
+
 	
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	
 
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
+	
+	
 }
+
+
+
+
+
