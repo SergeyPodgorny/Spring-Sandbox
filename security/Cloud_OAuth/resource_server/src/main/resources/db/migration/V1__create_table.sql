173865b464b55727f3e0a8ad5b5e0f058aa19e48
SergeyPodgorny <serkonpod@gmail.com>
2022-08-15 19:47:39 +0300

    migrations are written

--- security/Cloud_OAuth/resource_server/src/main/resources/db/migration/V1__create_table.sql
+++ security/Cloud_OAuth/resource_server/src/main/resources/db/migration/V1__create_table.sql
@@ -1,5 +1,27 @@
-CREATE TABLE IF NOT EXISTS accounts
-(id bigint, username varchar, password varchar);
+CREATE TABLE IF NOT EXISTS games_catalogue
 
-CREATE TABLE IF NOT EXISTS resources
-(id bigint, resource_message varchar);
+(game_id varchar(255) PRIMARY KEY UNIQUE NOT NULL,
+
+name varchar(255) UNIQUE NOT NULL,
+
+genre varchar(255) NOT NULL,
+
+release_date varchar(50) NOT NULL,
+
+developer varchar(200) NOT NULL,
+
+publisher varchar(200) NOT NULL,
+
+platform varchar(200) NOT NULL,
+
+features varchar(255) NOT NULL,
+
+price numeric(10,2) NOT NULL,
+
+discount numeric(10,2) NOT NULL,
+
+description varchar NOT NULL,
+
+base64Image varchar NOT NULL);
+
+
