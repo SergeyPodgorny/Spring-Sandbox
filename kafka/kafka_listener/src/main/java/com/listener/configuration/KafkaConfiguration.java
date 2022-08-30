f20b46d9e985c721800da26a44fa059a93b11f60
SergeyPodgorny <serkonpod@gmail.com>
2022-08-30 14:29:45 +0300

    added configuration to kafka producer and listener

--- kafka/kafka_listener/src/main/java/com/listener/configuration/KafkaConfiguration.java
+++ kafka/kafka_listener/src/main/java/com/listener/configuration/KafkaConfiguration.java
@@ -3,35 +3,35 @@ package com.listener.configuration;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
+@EnableKafka
 @Configuration
 public class KafkaConfiguration {
 
 	@Bean
-	public ProducerFactory <String,String> producerFactory(){
+	public ConsumerFactory<String,String> consumerFactory(){
 		Map<String,Object> kafkaConfig = new HashMap<>();
-		kafkaConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		kafkaConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		kafkaConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		return new DefaultKafkaProducerFactory<>(kafkaConfig);
+		kafkaConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		kafkaConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		kafkaConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		kafkaConfig.put(ConsumerConfig.GROUP_ID_CONFIG,"group-id");
+		return new DefaultKafkaConsumerFactory<>(kafkaConfig);
 	}
-	
 	
 	@Bean
-	public KafkaTemplate<String,String> kafkaTemplate(){
-		return new KafkaTemplate<>(producerFactory());
+	public ConcurrentKafkaListenerContainerFactory<String,String> kafkaListener(){
+		ConcurrentKafkaListenerContainerFactory<String,String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+		return factory;
 	}
-			
-	
-	
-	
-	
+
 	
 }
--- kafka/kafka_producer/src/main/java/com/producer/KafkaProducerApplication.java
+++ kafka/kafka_producer/src/main/java/com/producer/KafkaProducerApplication.java
@@ -1,4 +1,4 @@
-package com.producer.kafka_producer;
+package com.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
